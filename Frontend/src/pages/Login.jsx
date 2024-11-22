56d7702f6d6a80ad13c28d9a93fba45ab3347aad
SajidMujawar03 <sajidmmujawar9460@gmail.com>
2024-11-22 18:36:51 +0530

    hgfjsd

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
        
         
         try {
-          const res =await fetch(`${BASE_URL}/auth/login`,
+          const res =await fetch(`/auth/login`,
             {method:'post',
             headers:{
                 "Content-Type":"application/json"
